85bbb48ac880fda7c92a8d4db9b3779e08185083
chase <chase@bryx.com>
2015-02-25 09:49:44 -0500

    Made the tile class abstract so it can be parents of the specific tile types

--- src/ChipsChallenge/Map/Tile.kt
+++ src/ChipsChallenge/Map/Tile.kt
@@ -6,5 +6,6 @@ import ChipsChallenge.Engine.ObjectBase
  * Created by chase on 2/25/15.
  */
 
-class Tile(imageSource: String) : ObjectBase(imageSource) {
+abstract class Tile(imageSource: String) : ObjectBase(imageSource) {
+
 }
