6dd493751a380760f882631d9885d033395286fd
chase <chase@bryx.com>
2015-02-25 14:35:32 -0500

    Added the KeyBindings class which takes keyCodes and binds them to specific actions

---
+++ src/ChipsChallenge/Engine/KeyBindings.kt
@@ -0,0 +1,38 @@
+package ChipsChallenge.Engine
+
+/**
+ * Created by chase on 2/25/15.
+ */
+
+data class KeyBindings {
+
+    val KEY_CODE_UP = 0
+    val KEY_CODE_DOWN = 1
+    val KEY_CODE_LEFT = 2
+    val KEY_CODE_RIGHT = 3
+
+    var up = false
+    var down = false
+    var left = false
+    var right = false
+
+    public fun keyPressed(keyCode: Int) {
+        when (keyCode) {
+            KEY_CODE_UP -> up = true
+            KEY_CODE_DOWN -> down = true
+            KEY_CODE_LEFT -> left = true
+            KEY_CODE_RIGHT -> right = true
+        }
+    }
+
+    public fun keyReleased(keyCode: Int) {
+        when (keyCode) {
+            KEY_CODE_UP -> up = false
+            KEY_CODE_DOWN -> down = false
+            KEY_CODE_LEFT -> left = false
+            KEY_CODE_RIGHT -> right = false
+        }
+    }
+
+
+}
