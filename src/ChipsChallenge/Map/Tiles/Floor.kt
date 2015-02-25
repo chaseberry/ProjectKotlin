fc6142a2182687b390cf62ae67c29dce2f8ba7f7
chase <chase@bryx.com>
2015-02-25 09:51:47 -0500

    Added a base floor tile

---
+++ src/ChipsChallenge/Map/Tiles/Floor.kt
@@ -0,0 +1,15 @@
+package ChipsChallenge.Map.Tiles
+
+import ChipsChallenge.Map.Tile
+
+/**
+ * Created by chase on 2/25/15.
+ */
+
+class Floor : Tile("floor.png") {
+
+    override fun onTick() {
+
+    }
+
+}
