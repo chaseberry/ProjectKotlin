66b8f9c893fa9511d348d1b12cc75e13c45edbec
chase <chase@bryx.com>
2015-02-26 10:06:25 -0500

    Added a new function that calculates if a unit can move to a given tile

--- src/ChipsChallenge/Engine/UnitBase.kt
+++ src/ChipsChallenge/Engine/UnitBase.kt
@@ -1,6 +1,7 @@
 package ChipsChallenge.Engine
 
 import java.awt.image.BufferedImage
+import ChipsChallenge.Map.Tile
 
 /**
  * Created by chase on 2/25/15.
@@ -8,5 +9,7 @@ import java.awt.image.BufferedImage
 abstract class UnitBase(var x: Int, var y: Int, imageName: String) : EngineObjectBase {
 
     val image: BufferedImage? = loadImage(imageName)
+
+    abstract fun canMoveToTile(tile: Tile): Boolean
 
 }
