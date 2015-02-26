1e914e6434adc0d6ae6b4c17302dab743c872349
chase <chase@bryx.com>
2015-02-25 19:46:43 -0500

    Added some base Unit stuff and the Player class

--- src/ChipsChallenge/Engine/UnitBase.kt
+++ src/ChipsChallenge/Engine/UnitBase.kt
@@ -1,8 +1,12 @@
 package ChipsChallenge.Engine
 
+import java.awt.image.BufferedImage
+
 /**
  * Created by chase on 2/25/15.
  */
-abstract class UnitBase(var x: Int, var y: Int) : EngineObjectBase {
+abstract class UnitBase(var x: Int, var y: Int, imageName: String) : EngineObjectBase {
+
+    val image: BufferedImage? = loadImage(imageName, this)
 
 }
---
+++ src/ChipsChallenge/Unit/Player.kt
@@ -0,0 +1,14 @@
+package ChipsChallenge.Unit
+
+import ChipsChallenge.Engine.UnitBase
+
+/**
+ * Created by chase on 2/25/15.
+ */
+
+class Player(x: Int, y: Int) : UnitBase(x, y, "") {
+
+    override fun onTick() {
+    }
+
+}
