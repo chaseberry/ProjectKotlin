ce357cebbe43a6aafc351296603e56b717faaa9e
chase <chase@bryx.com>
2015-02-26 21:06:25 -0500

    Added a Lock object

--- src/ChipsChallenge/Engine/ObjectBase.kt
+++ src/ChipsChallenge/Engine/ObjectBase.kt
@@ -1,11 +1,12 @@
 package ChipsChallenge.Engine
 
 import java.awt.image.BufferedImage
+import ChipsChallenge.Map.Point
 
 /**
  * Created by chase on 2/25/15.
  */
-abstract class ObjectBase(imageSource: String?) : EngineObjectBase {
+abstract class ObjectBase(var location: Point, imageSource: String?) : EngineObjectBase {
 
     public open val image: BufferedImage? = loadImage(imageSource)
 
---
+++ src/ChipsChallenge/Object/Lock.kt
@@ -0,0 +1,16 @@
+package ChipsChallenge.Object
+
+import ChipsChallenge.Engine.ObjectBase
+import ChipsChallenge.Engine.Engine
+import ChipsChallenge.Map.Point
+
+/**
+ * Created by chase on 2/26/15.
+ */
+class Lock(location: Point, imageSource: String?) : ObjectBase(location, imageSource) {
+
+    override fun onTick(engine: Engine) {
+
+    }
+
+}
