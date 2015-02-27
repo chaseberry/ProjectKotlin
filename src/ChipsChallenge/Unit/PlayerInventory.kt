09128a984d0463804d70b51f6001040b8ebb6c36
chase <chase@bryx.com>
2015-02-27 09:26:40 -0500

    Updated the Inventory to hold the number of chips collected.

--- src/ChipsChallenge/Unit/PlayerInventory.kt
+++ src/ChipsChallenge/Unit/PlayerInventory.kt
@@ -6,4 +6,4 @@ package ChipsChallenge.Unit
 data class PlayerInventory(var hasFireBoots: Boolean = false, var hasIceSkates: Boolean = false,
                            var hasFlippers: Boolean = false, var hasScutionBoots: Boolean = false,
                            var redKeys: Int = 0, var blueKeys: Int = 0, var yellowKeys: Int = 0,
-                           var hasGreenKey: Boolean = false)
+                           var hasGreenKey: Boolean = false, var chipsCollected: Int = 0)
