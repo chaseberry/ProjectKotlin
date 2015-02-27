0a2edd936daf6c63319517091bd1b2b1dd38e2b4
chase <chase@bryx.com>
2015-02-27 17:40:13 -0500

    Added the Object Panel. Set the pallet status to the last selected item

--- src/ChipsChallenge/Editor/PalletPanes/ObjectPanel.kt
+++ src/ChipsChallenge/Editor/PalletPanes/ObjectPanel.kt
@@ -1,40 +1,41 @@
 package ChipsChallenge.Editor.PalletPanes
 
 import javax.swing.JPanel
-import ChipsChallenge.Map.tileIdToTile
 import javax.swing.JButton
-import ChipsChallenge.Map.Tile
 import javax.swing.ImageIcon
 import ChipsChallenge.Editor.EditorPallet
+import ChipsChallenge.Engine.objectFromId
+import ChipsChallenge.Map.Point
+import ChipsChallenge.Engine.ObjectBase
 import ChipsChallenge.Editor.PalletStatus
 
 /**
  * Created by chase on 2/27/15.
  */
-class TilePanel(val editorPallet: EditorPallet) : JPanel() {
+class ObjectPanel(val editorPallet: EditorPallet) : JPanel() {
 
-    val buttons = Array(2) { tileIdToTile(it) };
+    val buttons = Array(10) { objectFromId(it, Point(0, 0)) };
     val gridSize = 5
 
     {
         setLayout(null)
         setSize(buttons.size() % gridSize * 32, buttons.size() / gridSize * 32)
         for (z in 0..buttons.size() - 1) {
-            add(createButton(buttons[z], z))
+            add(createButton(buttons[z] as ObjectBase, z))
         }
         setVisible(true)
     }
 
-    fun createButton(tile: Tile, x: Int): JButton {
+    fun createButton(obj: ObjectBase, x: Int): JButton {
         val button = JButton()
         button.setLocation((x % gridSize) * 32, (x / gridSize) * 32)
         button.setSize(32, 32)
         button.setContentAreaFilled(false)
         button.setBorder(null)
-        button.setIcon(ImageIcon(tile.image))
+        button.setIcon(ImageIcon(obj.image))
         button.addActionListener {
-            editorPallet.currentTile = tile
-            editorPallet.palletStatus = PalletStatus.TILE
+            editorPallet.currentObject = obj
+            editorPallet.palletStatus = PalletStatus.OBJECT
         }
         return button
     }
