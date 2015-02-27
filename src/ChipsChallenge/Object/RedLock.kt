3787d3ebbda8a99045c0b733d888474e4e7708e9
chase <chase@bryx.com>
2015-02-27 09:20:16 -0500

    Updated the ObjectBase to have an interact that uses the engine to resolve if the interation was successful or not.

--- src/ChipsChallenge/Object/RedLock.kt
+++ src/ChipsChallenge/Object/RedLock.kt
@@ -7,7 +7,11 @@ import ChipsChallenge.Map.Point
 /**
  * Created by chase on 2/26/15.
  */
-class Lock(location: Point, imageSource: String?) : ObjectBase(location, imageSource) {
+class RedLock(location: Point, imageSource: String?) : ObjectBase(location, imageSource) {
+
+    override fun interact(engine: Engine): Boolean {
+
+    }
 
     override fun onTick(engine: Engine) {
 
