ad3cf0638dba2a2c83fcb2cbf7c19ef6fcd347ee
chase <chase@bryx.com>
2015-02-27 12:06:34 -0500

    Added a class to handle mouse clicks.

---
+++ src/ChipsChallenge/Editor/MouseBindings.kt
@@ -0,0 +1,28 @@
+package ChipsChallenge.Editor
+
+/**
+ * Created by chase on 2/27/15.
+ */
+class MouseBindings() {
+
+    val MOUSE_ONE = 1
+    val MOUSE_TWO = 3
+
+    var mouseOne = false
+    var mouseTwo = false
+
+    fun mousePressed(keyCode: Int) {
+        when (keyCode) {
+            MOUSE_ONE -> mouseOne = true
+            MOUSE_TWO -> mouseTwo = true
+        }
+    }
+
+    fun mouseReleased(keyCode: Int) {
+        when (keyCode) {
+            MOUSE_ONE -> mouseOne = false
+            MOUSE_TWO -> mouseTwo = false
+        }
+    }
+
+}
