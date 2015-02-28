f23babdcf74a507dbdee58a88e2c0ef056f81971
chase <chase@bryx.com>
2015-02-28 09:03:24 -0500

    Changed the file texture of Help to use the right image

--- src/ChipsChallenge/Map/Tiles/Help.kt
+++ src/ChipsChallenge/Map/Tiles/Help.kt
@@ -6,9 +6,8 @@ import ChipsChallenge.Engine.Engine
 /**
  * Created by chase on 2/28/15.
  */
-class Help:Tile("help.gif", 3){
+class Help : Tile("hint.gif", 3) {
     override fun onTick(engine: Engine) {
-        throw UnsupportedOperationException()
     }
 
 }
